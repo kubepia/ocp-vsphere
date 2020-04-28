1ca1f93c80f9d6e40a0726dec5ded0a30e629205
myguddy <seungil.han@gmail.com>
2020-04-28 20:29:22 +0900

    bootstrap

---
+++ main.tf
@@ -0,0 +1,77 @@
+locals {
+  bootstrap_fqdns     = ["bootstrap-0.${var.cluster_domain}"]
+  control_plane_fqdns = [for idx in range(var.control_plane_count) : "control-plane-${idx}.${var.cluster_domain}"]
+  compute_fqdns       = [for idx in range(var.compute_count) : "compute-${idx}.${var.cluster_domain}"]
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "compute_cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vm_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vm_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_resource_pool" "resource_pool" {
+  name                    = var.cluster_id
+  parent_resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
+}
+
+resource "vsphere_folder" "folder" {
+  path          = var.cluster_id
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+module "bootstrap" {
+  source = "./vm"
+
+  ignition = file(var.bootstrap_ignition_path)
+
+  hostnames_ip_addresses = {
+      local.bootstrap_fqdns = var.bootstrap_ip_address
+  }
+#   zipmap(
+#     local.bootstrap_fqdns,
+#     module.ipam_bootstrap.ip_addresses
+#   )
+
+  resource_pool_id      = vsphere_resource_pool.resource_pool.id
+  datastore_id          = data.vsphere_datastore.datastore.id
+  datacenter_id         = data.vsphere_datacenter.dc.id
+  network_id            = data.vsphere_network.network.id
+  folder_id             = vsphere_folder.folder.path
+  guest_id              = data.vsphere_virtual_machine.template.guest_id
+  template_uuid         = data.vsphere_virtual_machine.template.id
+  disk_thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
+
+  cluster_domain = var.cluster_domain
+  machine_cidr   = var.machine_cidr
+
+  num_cpus      = 8
+  memory        = 16384
+  dns_addresses = var.vm_dns_addresses
+}
---
+++ variables.tf
@@ -0,0 +1,91 @@
+//////
+// vSphere variables
+//////
+
+variable "vsphere_server" {
+  type        = string
+  description = "This is the vSphere server for the environment."
+}
+
+variable "vsphere_user" {
+  type        = string
+  description = "vSphere server user for the environment."
+}
+
+variable "vsphere_password" {
+  type        = string
+  description = "vSphere server password"
+}
+
+variable "vsphere_cluster" {
+  type        = string
+  description = "This is the name of the vSphere cluster."
+}
+
+variable "vsphere_datacenter" {
+  type        = string
+  description = "This is the name of the vSphere data center."
+}
+
+variable "vsphere_datastore" {
+  type        = string
+  description = "This is the name of the vSphere data store."
+}
+
+variable "vm_template" {
+  type        = string
+  description = "This is the name of the VM template to clone."
+}
+
+variable "vm_network" {
+  type        = string
+  description = "This is the name of the publicly accessible network for cluster ingress and access."
+  default     = "VM Network"
+}
+
+variable "vm_dns_addresses" {
+  type    = list(string)
+  default = ["1.1.1.1", "9.9.9.9"]
+}
+
+/////////
+// OpenShift cluster variables
+/////////
+
+variable "cluster_id" {
+  type        = string
+  description = "This cluster id must be of max length 27 and must have only alphanumeric or hyphen characters."
+}
+
+variable "base_domain" {
+  type        = string
+  description = "The base DNS zone to add the sub zone to."
+}
+
+variable "cluster_domain" {
+  type        = string
+  description = "The base DNS zone to add the sub zone to."
+}
+
+variable "machine_cidr" {
+  type = string
+}
+
+/////////
+// Bootstrap machine variables
+/////////
+
+variable "bootstrap_ignition_path" {
+  type    = string
+  default = "./bootstrap.ign"
+}
+
+variable "bootstrap_complete" {
+  type    = string
+  default = "false"
+}
+
+variable "bootstrap_ip_address" {
+  type    = string
+  default = ""
+}
